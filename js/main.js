2c332e714617afe3e73bba564b284259f1b0ff4e
Diman Todorov <diman.todorov@outlook.com>
2016-09-03 11:43:02 -0700

    project scaffold

---
+++ js/main.js
@@ -0,0 +1,10 @@
+require("../style/style.scss")
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('App')
+);
+
---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    entry: "./js/main.js",
+    output: {
+        publicPath: "http://localhost:8080/js/",
+        path: "./js/",
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [ 
+            { test: /\.scss$/, loaders: ["style", "css", "sass"] },
+            { test: /\.js$/, exclude: /(node_modules)/, loader: 'babel', query:{ presets:['react']} }
+        ]
+    }
+};
